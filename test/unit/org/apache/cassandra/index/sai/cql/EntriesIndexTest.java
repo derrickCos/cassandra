1c2da7c9c324b2dfd3f9b211a42a05ffff26e038
Michael Marshall <mmarshall@apache.org>
2023-10-16 13:57:55 -0500

    Add equality test for negative values

--- test/unit/org/apache/cassandra/index/sai/cql/EntriesIndexTest.java
+++ test/unit/org/apache/cassandra/index/sai/cql/EntriesIndexTest.java
@@ -31,12 +31,20 @@ public class EntriesIndexTest extends SAITester
         createIndex("CREATE CUSTOM INDEX ON %s(entries(item_cost)) USING 'StorageAttachedIndex'");
         waitForIndexQueryable();
 
-        execute("INSERT INTO %s (partition, item_cost) VALUES (1, {'apple': 1, 'orange': 2})");
+        execute("INSERT INTO %s (partition, item_cost) VALUES (1, {'apple': 1, 'orange': -2})");
         flush();
         execute("INSERT INTO %s (partition, item_cost) VALUES (2, {'apple': 2, 'orange': 1})");
         execute("INSERT INTO %s (partition, item_cost) VALUES (3, {'apple': 1, 'orange': 3})");
+        execute("INSERT INTO %s (partition, item_cost) VALUES (4, {'apple': 10, 'orange': -7})");
 
+        // Test equality over both sstable and memtable
         assertRows(execute("SELECT partition FROM %s WHERE item_cost['apple'] = 1"), row(1), row(3));
+        // Test sstable read
+        assertRows(execute("SELECT partition FROM %s WHERE item_cost['orange'] = -2"), row(1));
+        // Test memtable read
+        assertRows(execute("SELECT partition FROM %s WHERE item_cost['orange'] = -7"), row(4));
+        // Test miss
+        assertRows(execute("SELECT partition FROM %s WHERE item_cost['orange'] = -3"));
     }
 
     @Test
